c8e10d58dad0cd0984d06eab588c5385c736bab7
“Snappyhacker” <jimodhiambo515@gmail.com>
2024-07-01 14:55:58 +0300

    arithmetic

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+// 1. Multiply
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+
+// 2. Random
+const random = Math.floor(Math.random() * 100) + 1; // Generates a random integer between 1 and 100
+
+// 3. Mod
+const num3 = 10;
+const num4 = 6;
+const mod = num3 % num4;
+
+// 4. Max
+const max = Math.max(1, 2, 3, 20);
